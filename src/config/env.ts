ce3dffd5cc37d61cd8ff10c824b1a5dbcf2248af
hassam-01 <ali.hassam1@outlook.com>
2025-05-13 13:04:01 +0500

    Fix: vercel config dotenv

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,8 @@
 
-if (typeof process !== "undefined" && process.env) {
-    const dotenv = await import("dotenv");
-    dotenv.config();
-}
+// if (typeof process !== "undefined" && process.env) {
+//     const dotenv = await import("dotenv");
+//     dotenv.config();
+// }
 
 export const imgBBAPI = import.meta.env.VITE_IMGBB_API_KEY || "";
 
